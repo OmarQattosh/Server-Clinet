cf2f92ed70c6d87a33efce8949ecb9cf6aae90be
OmarQattosh <76616260+OmarQattosh@users.noreply.github.com>
2021-08-02 09:19:36 +0300

    Update killme.sh

--- server/killme.sh
+++ server/killme.sh
@@ -1,5 +1,5 @@
 docker build -t test .
-docker run -d -p 8899:80  test
+docker run -d -p 8899:81  test
 docker login -u "omarqattosh" -p "omar159852357" docker.io
 docker tag test omarqattosh/server
 docker push omarqattosh/server
